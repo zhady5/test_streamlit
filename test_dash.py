bcbd4ed3b6d8959f2f8c414deb5d8099307d019d
Mika <42176090+zhady5@users.noreply.github.com>
2024-12-30 20:11:53 +0500

    Update test_dash.py

--- test_dash.py
+++ test_dash.py
@@ -4,16 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Функция для изменения фона с помощью JavaScript
-def set_bg_hack(main_color):
-    max_characters = 5000  # Максимальное количество символов для заполнения
-    snippet = f""" <style> .reportview-container {{ background: {main_color} !important; }} .sidebar-content {{ background: {main_color} !important; }} </style> """
-    return snippet + " " * max_characters
+# Создание контейнера для всего содержимого
+container = st.container()
 
-# Применяем фоновый цвет
-st.markdown(set_bg_hack('#ffb347'), unsafe_allow_html=True)
+# Настраиваем стили для фона и отступов
+container.markdown(
+    f"""
+    <style>
+        .css-1v4kyyz {{
+            background-color: #ffb347 !important;
+            margin-left: 10% !important;  /* Отступ слева */
+            margin-right: 10% !important; /* Отступ справа */
+            padding: 0 !important;
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Заголовок
-st.title("Мой Дашборд")
+container.title("Мой Дашборд")
 
 # Генерируем случайные данные для графиков
 np.random.seed(42)
@@ -28,51 +38,51 @@ data = pd.DataFrame({
 })
 
 # Индикаторы
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3, col4 = container.columns(4)
 with col1:
-    st.metric(label="Индикатор 1", value=np.random.randint(500))
+    container.metric(label="Индикатор 1", value=np.random.randint(500))
 with col2:
-    st.metric(label="Индикатор 2", value=np.random.randint(200))
+    container.metric(label="Индикатор 2", value=np.random.randint(200))
 with col3:
-    st.metric(label="Индикатор 3", value=np.random.randint(300))
+    container.metric(label="Индикатор 3", value=np.random.randint(300))
 with col4:
-    st.metric(label="Индикатор 4", value=np.random.randint(400))
+    container.metric(label="Индикатор 4", value=np.random.randint(400))
 
 # Разделение на три строки для графиков
-row1, row2, row3 = st.columns((1, 1, 1))
+row1, row2, row3 = container.columns((1, 1, 1))
 
 # Первый ряд графиков
 with row1:
     fig1, ax1 = plt.subplots()
     sns.lineplot(x=data['x'], y=data['y1'], ax=ax1)
-    st.pyplot(fig1)
+    container.pyplot(fig1)
     
     fig2, ax2 = plt.subplots()
     sns.barplot(x=data['x'], y=data['y2'], ax=ax2)
-    st.pyplot(fig2)
+    container.pyplot(fig2)
 
 # Второй ряд графиков
 with row2:
     fig3, ax3 = plt.subplots()
     sns.scatterplot(x=data['x'], y=data['y3'], ax=ax3)
-    st.pyplot(fig3)
+    container.pyplot(fig3)
     
     fig4, ax4 = plt.subplots()
     sns.histplot(data['y4'], kde=True, ax=ax4)
-    st.pyplot(fig4)
+    container.pyplot(fig4)
 
 # Третий ряд графиков
 with row3:
     fig5, ax5 = plt.subplots()
     sns.boxplot(y=data['y5'], ax=ax5)
-    st.pyplot(fig5)
+    container.pyplot(fig5)
     
     fig6, ax6 = plt.subplots()
     sns.kdeplot(data['y6'], shade=True, ax=ax6)
-    st.pyplot(fig6)
+    container.pyplot(fig6)
 
 # Дополнительное описание
-st.write("""
+container.write("""
 ### Описание данных
 Это тестовые данные, которые были сгенерированы случайным образом для демонстрации возможностей дашборда.
 """)
